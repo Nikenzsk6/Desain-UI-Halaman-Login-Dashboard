2397d038ccf029f944a0019b696ee4855b43f9ab
Nikenzsk6 <ajaniken307@gmail.com>
2025-06-08 07:20:00 +0700

    class ALumni_202457201057

---
+++ src/alumni_202457201057/Alumni_202457201057.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alumni_202457201057;
+
+/**
+ *
+ * @author HP
+ */
+public class Alumni_202457201057 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
